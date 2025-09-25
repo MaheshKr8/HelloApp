c44c868d3425bc52d286e7bfd6baa900a404e542
MaheshKr8 <kr99mahesh@gmail.com>
2025-09-25 19:03:37 +0530

    AKS name added for deployment

---
+++ infra/AKS/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "d199062a-9d18-43ed-bee5-666c1b771e79"
+}
+
